113592f89ac8e8ab3293019b938f929094561c0e
Bountx <andrzej.pijanowski@gmail.com>
2026-02-26 22:35:48 +0000

    feat: add CMake configuration and main application with Boost and simdjson integration

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <boost/asio.hpp>
+#include <boost/beast.hpp>
+#include "simdjson.h"
+
+int main() {
+    std::cout << "Engine starting: Boost and simdjson successfully linked!" << std::endl;
+    return 0;
+}
